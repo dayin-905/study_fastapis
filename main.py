753d075c8090faa53d6ebf16c9de4b1dca9625fa
Lia <dayin.9905@gmail.com>
2025-11-24 14:39:04 +0900

    00

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ from fastapi.templating import Jinja2Templates # fast api 템플릿 사용
 from fastapi import Request # 외부 요청이기 때문에 파라미터로 들어감.
 templates = Jinja2Templates(directory="templates/") # 클래스 변수(함수 담는 값) = Jinja 템플릿 html경로 설정
 
+# index.html
+@app.get("/") 
+async def main_html(request : Request): 
+    return templates.TemplateResponse("index.html", {"request" : request})
+
 # admin.html
 @app.get("/admin_html") # 네트워크용 함수 호출
 async def main_html(request : Request): # 파일 호출
@@ -16,10 +21,5 @@ async def main_html(request : Request): # 파일 호출
 @app.get("/bakery_html") 
 async def main_html(request : Request): 
     return templates.TemplateResponse("bakery.html", {"request" : request})
-
-# index.html
-@app.get("/") 
-async def main_html(request : Request): 
-    return templates.TemplateResponse("index.html", {"request" : request})
 
 pass
