40d2d8d3b61369411db0c4bb382f2e100e5650d1
Lia <dayin.9905@gmail.com>
2025-11-25 14:48:48 +0900

    00

---
+++ querys/todos.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS todo (
+            id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+            item VARCHAR(255) NOT NULL
+        )
+
+INSERT INTO todo (item)
+VALUES ('Learn SQL') :
+
+SELECT id, item
+FROM todo :
+
+UPDATE todo
+SET item = 'Learn Advanced SQL'
+WHERE id = '3c8b05aa-9893-4e9f-be4b-3a969e7fc726'
