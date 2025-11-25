c1cc7cbb2010cc15828ee573a2d66ce4633e8934
Lia <dayin.9905@gmail.com>
2025-11-25 17:05:07 +0900

    00

--- querys/todos.sql
+++ querys/todos.sql
@@ -7,9 +7,12 @@ INSERT INTO todo (item)
 VALUES ('Learn SQL'), ('Build a REST API'), ('Write Unit Tests.');
 
 SELECT id, item
-FROM todo;
+FROM todo
+WHERE id = '22bc5937-fd03-4337-830f-374b337f7e03';
+
+SELECT *
+FROM todo
 
 UPDATE todo
 SET item = 'Learn Advanced SQL'
-WHERE id = '3c8b05aa-9893-4e9f-be4b-3a969e7fc726'
-
+WHERE id = '3c8b05aa-9893-4e9f-be4b-3a969e7fc726';
--- routes/todos.py
+++ routes/todos.py
@@ -9,6 +9,23 @@ router = APIRouter()
 templates = Jinja2Templates(directory="templates/")
 
 # http://localhost:8000/todos/
+@router.get("/{todo_id}")
+def get_todo(request : Request, todo_id : str):
+    conn = get_db_connection() # DB 연결 테스트 통로
+    with conn.cursor(cursor_factory=DictCursor) as cursor:
+        cursor.execute(f"""SELECT id, item
+                        FROM todo
+                        WHERE id = '{todo_id}';""")
+        todo = cursor.fetchone() # 결과를 하나만 가져옴.
+    conn.close()
+
+    context = {
+        "request" : request,
+        "todo" : todo
+    }
+    return templates.TemplateResponse("todos/merged_todo.html", context)
+
+# http://localhost:8000/todos/
 @router.get("/")
 def get_todos_html(request : Request) :
     conn = get_db_connection() # DB 연결 테스트 통로
@@ -22,3 +39,4 @@ def get_todos_html(request : Request) :
         "todos" : todos # cursor_factory를 해줘서 dict 형식으로 바뀜
     }
     return templates.TemplateResponse("todos/merged_todo.html", context)
+
